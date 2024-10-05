5dbcabfdceeba37e15ae14679965c33f883ca4cd
Titan9200 <blackjoker9002@gmail.com>
2024-10-05 19:56:00 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
 st.title('😎 Khusrav')
 
