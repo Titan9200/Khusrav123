c94f869052fc48e463e52074d8796fa1b189c53e
Titan9200 <blackjoker9002@gmail.com>
2024-10-05 19:11:29 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,14 +20,13 @@ with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm',y='body_mass_g',color='species')
   st.scatter_chart(data=df, x='bill_depth_mm', y='sex', color='species')
 
-with st.sidebar:
-  st.header('input features')
-  island = st.selection('')
-  bill_length_mm = st.slider('Bill length {mm}', 32.1, 59.6, 43.9)
-  bill_depth_mm = st.slider('Bill length {mm}' 13.1,21.5,17.2)
-  flipper_length_mm = st.slider()
-  body_nass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
-  gender = st.selectbox('Gender', ('male','female'))
+  st.header('Inout Features')
+  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgerson'))
+  bill_length_mm = st.slider('Bill depth (mm)', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2) 
+  flipper_length_mm = st.slider('flipper length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+  gender = st.selectbox('Gender', ('male', 'female'))
 
 # Create a DataFrame for the input features
   data = {'island': island,
