cdceaa2386d30ffe2f167e1e5548e55e503caa66
Titan9200 <blackjoker9002@gmail.com>
2024-10-05 19:26:33 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,20 +39,20 @@ with st.expander('Data visualization'):
   input_penguins = pd.concat([input_df, X_raw], axis=0)
 
 encode = ['island', 'sex']
-df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+df_penguins = pd.get_dummies (input_penguins, prefix=encode)
 
-X = df_penguins[:1]
-
-target_napper = {'Adelie': 0,
+X = df_penguins [1:]
+input_row = df_penguins [:1]
+# Encode y
+target_mapper = {'Adelie': 0,
                  'Chinstrap': 1,
                  'Gentoo': 2}
 def target_encode(val):
-  return target_mapper[val]
-
+  return target_mapper [val]
 y = y_raw.apply(target_encode)
-
-with st.expander('Data preparation'):
-  st.write('**incoded X (input penguin)**')
+with st.expander ('Data preparation'):
+  st.write('**Encoded X (input penguin)**')
   input_row
   st.write('**Encoded y**')
   y
+
